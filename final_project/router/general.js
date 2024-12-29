f5afce2e5923506f944d75cc2869a63b06dc8edc
Tung Nguyen <nvntung@gmail.com>
2024-12-29 13:05:48 +0000

    Task 2: Get Book Details By ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -15,11 +15,33 @@ public_users.get('/',function (req, res) {
   res.send(books);
 });
 
+function filterBooksUsingTypicalLoop(isbn) {
+  let foundBooks = [];
+  let book;
+  for (let bookId in books) {
+    book = books[bookId];
+    // console.log(book);
+    if (book["isbn"] === isbn) {
+      foundBooks.push(book);
+    }
+  }
+  return foundBooks;
+}
+
+function filterBooksIteratingOverDictionary(isbn) {
+  const _books = Object.values(books);
+  const foundBooks = _books.filter((book) => book.isbn === isbn);
+  return foundBooks;
+}
+
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  
+  // const foundBooks = filterBooksUsingTypicalLoop(isbn);
+  const foundBooks = filterBooksIteratingOverDictionary(isbn);
+  res.send(foundBooks);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
